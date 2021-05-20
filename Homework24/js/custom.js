79735274c7f12502ebc25262a25276fd92a35ff5
Dmitry Shovkoplias <dmitryshovk@gmail.com>
2021-05-20 16:22:15 +0300

    fixed bugs2

--- Homework24/js/custom.js
+++ Homework24/js/custom.js
@@ -1,27 +1,26 @@
 let block = document.querySelector('.block');
 const requestURL = 'https://api.github.com/users';
-
-function sendRequest(url) {
-  return fetch(url)
+function sendRequest() {
+  return fetch(requestURL)
     .then(response => {
       let users = response.json()
       return users
     })
     .then((parseUser) => {
-      createdPosts(parseUser, block)
+      for (let i = 0; i < 10; i++) {
+        let blockwrap = document.createElement('li');
+        let newPost = `<div class="flex">
+                        <img src="${parseUser[i]['avatar_url']}"></img>
+                        <div class="text-wrap">
+                          <span>login: ${parseUser[i]['login']}</span>
+                          <span>id: ${parseUser[i]['id']}</span>
+                        <div>
+                      </div>`;
+        blockwrap.innerHTML = newPost;
+        block.appendChild(blockwrap);
+      }
+    }).catch((e) => {
+      block.innerHTML = `<div class="wrong-url">Проверьте ввод данных</div>`;
     })
 }
-sendRequest(requestURL)
-
-function createdPosts(data, b) {
-  for (let i = 0; i < 10; i++) {
-    let blockwrap = document.createElement('li');
-    let newPost = `<div>
-                    <span>login: ${data[i]['login']}</span>
-                    <span>id: ${data[i]['id']}</span>
-                    <span>User Avatar:<a href="${data[i]['avatar_url']}">${data[i]['avatar_url']}</a></span>
-                  </div>`
-    blockwrap.innerHTML = newPost;
-    b.appendChild(blockwrap);
-  }
-}
+sendRequest()
